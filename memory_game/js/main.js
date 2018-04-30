b99aeb71de4adbaa812ef9c9a0b82ba6c1b244d3
Jesse Brito <jesse@jesses-mbp.home>
2018-04-30 15:17:26 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,43 @@
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+
+var cardsInPlay = [];
+
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[2]) {
+		console.log("You found a match!");
+	} else {
+		alert: "Sorry, try again";
+		console.log("Sorry, try again.");
+	}
+}
+
+var flipCard = function (cardId) {
+	checkForMatch();
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+}
+
+console.log(cards.cardImage);
+console.log(cards.suit);
